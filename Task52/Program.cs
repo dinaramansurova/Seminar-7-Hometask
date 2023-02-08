a878a6476063c2590920f983261dc6f365741974
Dinara <ash.solarium@gmail.com>
2023-02-08 16:08:27 +0300

    tasks

---
+++ Task52/Program.cs
@@ -0,0 +1,42 @@
+﻿// Task 52
+// Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+void InputMatrix(double[,] matrix)
+{
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+for (int j = 0; j < matrix.GetLength(1); j++)
+{
+matrix[i, j] = Math.Round(new Random().NextDouble() * (20 + 20) - 20, 2);
+Console.Write($"{matrix[i, j]} \t");
+}
+Console.WriteLine();
+}
+}
+
+
+void ReleaseMatrix(double[,] matrix)
+{
+for (int j = 0; j < matrix.GetLength(1); j++)
+{
+double summa = 0;
+for(int i = 0; i < matrix.GetLength(0); i++)
+{
+summa += matrix[i, j];
+}
+Console.WriteLine($"Результат ср. ариф. {j + 1} = {summa / matrix.GetLength(0)}");
+}
+}
+
+
+Console.Clear();
+Console.Write("Введите размеры массива: ");
+int[] size = Console.ReadLine().Split().Select(x => int.Parse(x)).ToArray();
+double[,] matrix = new double[size[0], size[1]];
+InputMatrix(matrix);
+ReleaseMatrix(matrix);
---
+++ task50/Program.cs
@@ -0,0 +1,43 @@
+﻿// Task 50
+// Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, 
+// и возвращает значение этого элемента или же указание, что такого элемента нет.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 1 7 -> такой позиции в массиве нет
+
+void InputMatrix(double[,] matrix)
+{
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+for (int j = 0; j < matrix.GetLength(1); j++)
+{
+matrix[i, j] = Math.Round(new Random().NextDouble() * (20 + 20) - 20, 2);
+Console.Write($"{matrix[i, j]} \t");
+}
+Console.WriteLine();
+}
+}
+
+
+double SearchElementToMatrix(double[,] matrix, int row, int column)
+{
+if (matrix.GetLength(0) > row && matrix.GetLength(1) > column && row >= 0 && column >= 0)
+return matrix[row, column];
+return 0;
+}
+
+
+Console.Clear();
+Console.Write("Введите размеры массива: ");
+int[] size = Console.ReadLine().Split().Select(x => int.Parse(x)).ToArray();
+double[,] matrix = new double[size[0], size[1]];
+InputMatrix(matrix);
+int[] coord = Console.ReadLine().Split().Select(x => int.Parse(x)).ToArray();
+Console.Write("Введите позицию элемента: ");
+double result = SearchElementToMatrix(matrix, coord[0] - 1, coord[1] - 1);
+if (result != 0)
+Console.WriteLine(result);
+else
+Console.WriteLine("Такой позиции в массиве нет");
